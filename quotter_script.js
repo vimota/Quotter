e0b30436e477b4e35d988e3001d10011f5041ba3
Victor Mota <vimota@gmail.com>
2013-05-14 17:05:36 -0400

    Works again

--- quotter_script.js
+++ quotter_script.js
@@ -2,12 +2,11 @@ $(function () {
   function handleClick(e){
     e.preventDefault();
     var quoteText, quoteAuthor, prefillText;
-
-    quoteText = $(this).parents('.stream-item-footer').siblings('.js-tweet-text').text().match(/[\S](.*)[\S]/)[0];
+    quoteText = $(this).parents('.stream-item-footer').siblings('.js-tweet-text').text();
     quoteAuthor = $(this).parents('.content').find('.username b').text();
 
     prefillText = '"@' + quoteAuthor + ': ' + quoteText + '"';
-    $('#tweet-box-mini-home-profile').focus().val(prefillText);
+    $('#tweet-box-mini-home-profile div').focus().text(prefillText);
   }
 
   var quoteHtml = '<li class="quotter-container"><a href="#"><b>Quote</b></a></li>',
